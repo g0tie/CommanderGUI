9e037c9fa7ac7ac8352d232e5dc7ad1743419107
g0tie <gaultierbegue@outlook.com>
2021-11-16 10:38:54 +0400

    rm clonerepoview + simplify interfaces + add command support

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Avalonia.Controls;
 using MyApp.Views;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace MyApp.ViewModels
 {
@@ -12,23 +14,93 @@ namespace MyApp.ViewModels
         {
             
         }
-        public string Greeting => "Welcome to Avalonia!";
-
-        private string _directoryPath = @"c:\Example";
-        public string DirectoryPath
+        private string _gitUrl = @"ssh://git@example.com/example.git";
+        public string GitUrl
         {
-            get => _directoryPath;
+            get => _gitUrl;
             set {
-                _directoryPath = value;
+                _gitUrl = value;
             }
         }
-        
-        private void OpenCloneRepoView()
+        private string _gitMsh = @"first commit";
+        public string GitMsg
         {
-            // System.Console.Write(DirectoryPath);
-            var cloneRepoWindow = new CloneRepoView();
-            cloneRepoWindow.DirPath = _directoryPath;
-            cloneRepoWindow.Show();
+            get => _gitMsh;
+            set {
+                _gitMsh = value;
+            }
         }
+
+        public async void folderOpener(Window window, string title, Action<string, string> callback, string command)
+        {
+            var folderDialog = new OpenFolderDialog();
+            folderDialog.Title = title;
+
+            var folder = await folderDialog.ShowAsync(window);
+
+            if (folder != null)
+            {
+               callback(folder, command);
+            }
+        }
+
+        //  public async void fileOpener(Window window, string title, Action<string, string> callback, string command)
+        // {
+        //     var fileDialog = new OpenFileDialog();
+        //     fileDialog.Title = title;
+
+        //     var file = await fileDialog.ShowAsync(window);
+
+        //     if (file != null)
+        //     {
+        //        callback(file, command);
+        //     }
+        // }
+
+        private void bashExec(string path, string cmd)
+        {
+            string command = $"cd {path} && {cmd}";
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{command}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+
+        // public void DeleteRepo(Window window) {
+        //     folderOpener(window,"Supprimer un dépot local", bashExec, "rm -rf");
+        // }
+
+        public void CloneRepo(Window window) {
+            folderOpener(window,"Sélectionner une destination", bashExec, $"git clone {GitUrl}");
+        }
+
+        public void SyncRepo(Window window) {
+            folderOpener(window,"Sélectionner un dépot à synchroniser", bashExec, "git pull ");
+        }
+
+
+        // public void AddFilesToRepo(Window window) {
+        //     folderOpener(window,"Sélectionner les fichiers à ajouter", bashExec, "git pull ");
+        // }
+
+        public void CommitRepo(Window window) {
+            folderOpener(window,"Sélectionner un dépot à synchroniser", bashExec, $"git commit -m {GitMsg}");
+        }
+
+        public void PushChangesToRepo(Window window) {
+            folderOpener(window,"Sélectionner un dépot à synchroniser", bashExec, $"git push");
+        }
+       
     }
 }
